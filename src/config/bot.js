faefb0e090866d1adf86a6a410b56ba19097ad3e
titobandito77 <tito77.messi@gmail.com>
2026-05-30 00:07:26 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Clannect on Top",
+        name: "Clannect on Top!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
